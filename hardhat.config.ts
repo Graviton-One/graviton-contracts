375f02cfc7464273e7f7f8aec34388cebed3c661
Anton Davydov <fetsorn@gmail.com>
2021-06-11 15:32:29 +0300

    build: add typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,18 @@
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname+'/.env' });
-// require('dotenv').config({ path: __dirname+'/.env' });
 
 import { task } from "hardhat/config";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
 import "solidity-coverage";
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
+  solidity: "0.8.0",
   networks: {
     hardhat: {
     },
@@ -55,5 +44,4 @@ export default {
   etherscan: {
     apiKey: process.env.APIKEY
   },
-  solidity: "0.8.0",
 };
