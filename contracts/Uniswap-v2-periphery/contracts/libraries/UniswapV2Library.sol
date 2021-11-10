55eb4ad60469ccbc6892f5750b52f12aba6302ef
badconfig <array.clean@gmail.com>
2021-11-10 15:52:32 +0400

    fix invoke

--- contracts/Uniswap-v2-periphery/contracts/libraries/UniswapV2Library.sol
+++ contracts/Uniswap-v2-periphery/contracts/libraries/UniswapV2Library.sol
@@ -32,14 +32,8 @@ library UniswapV2Library {
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
-    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
-        (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(uint(keccak256(abi.encodePacked(
-                hex'ff',
-                factory,
-                keccak256(abi.encodePacked(token0, token1)),
-                hex'e95ab727956d450789856640b33d17659b8d833fdd2e099814ce92bf0817cc8f' // init code hash
-            ))));
+    function pairFor(address factory, address tokenA, address tokenB) internal view returns (address pair) {
+        pair = IUniswapV2FactoryT(factory).getPair(tokenA,tokenB);
     }
 
     // fetches and sorts the reserves for a pair
