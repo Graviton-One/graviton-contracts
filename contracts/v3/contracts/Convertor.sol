52c4466230a55342d1bd4d18e11e269f417a0027
googrand <sharkanovnikita@gmail.com>
2021-11-12 01:25:03 +0600

    created fixtures

--- contracts/v3/contracts/Convertor.sol
+++ contracts/v3/contracts/Convertor.sol
@@ -1,18 +1,18 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import "./interfaces/IWormhole.sol";
+import "./interfaces/IConvertor.sol";
 import "./interfaces/IERC20.sol";
 
 
-contract Wormhole is IWormhole {    
+contract Convertor is IConvertor {    
     address public owner;
-    address public price;
+    uint256 public price;
 
-    bool public override swapActivated;
-    bool public override limitActivated;
+    bool public swapActivated;
+    bool public limitActivated;
 
-    IERC20 public wallet;
+    address public wallet;
     IERC20 public gton;
 
     modifier isOwner() {
@@ -20,32 +20,32 @@ contract Wormhole is IWormhole {
         _;
     }
 
-    function setOwner(address _owner) public override isOwner {
+    function setOwner(address _owner) public isOwner {
         address ownerOld = owner;
         owner = _owner;
         emit SetOwner(ownerOld, _owner);
     }
 
-    function setWallet(address _wallet) public override isOwner {
+    function setWallet(address _wallet) public isOwner {
         address walletOld = wallet;
         wallet = _wallet;
         emit SetWallet(walletOld, _wallet);
     }
 
-    function setPrice(uint _price) public onlyOwner {
+    function setPrice(uint _price) public isOwner {
         uint priceOld = price;
         price = _price;
         emit SetPrice(priceOld, _price);
     }
 
-    function calcAmountOut(uint amount) internal returns (uint) {
+    function calcAmountOut(uint amount) internal pure returns (uint) {
 
     }
 
-    function swap(uint amount) public returns (uint) {
+    function convert(uint amount) public override {
         require(gton.transferFrom(msg.sender, address(this), amount), "Not enought of allowed gton.");
-        uint amountOut = calcAmountOut(amount);
-        gton.transferFrom(wallet, msg.sender, amountOut);
-        emit Swap(msg.sender, msg.sender, amount, amountOut);
+        // uint amountOut = calcAmountOut(amount);
+        // gton.transferFrom(wallet, msg.sender, amountOut);
+        // emit Swap(msg.sender, msg.sender, amount, amountOut);
     }
 }
