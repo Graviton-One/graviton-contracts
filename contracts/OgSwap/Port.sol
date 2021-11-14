5cc123effd599375824d87a459d39f704bde9150
badconfig <array.clean@gmail.com>
2021-11-14 04:27:11 +0400

    fix

--- contracts/OgSwap/Port.sol
+++ contracts/OgSwap/Port.sol
@@ -4,71 +4,8 @@ pragma solidity >=0.8.0;
 import "./interfaces/ierc20.sol";
 import "./interfaces/uniswapRouter.sol";
 import "./interfaces/weth.sol";
-import "./interfaces/wormhole.sol";
 
 contract OGSwap {
-    IWETH public eth;
-    IERC20 public gtonToken;
-    IUniswapV2Router02 public router;
-    IUniswapV2Factory public factory;
-    Wormhole public wormhole;
-    address public owner;
-    bool public revertFlag;
-    
-    modifier onlyOwner() {
-        require(msg.sender==owner,'not permitted');
-        _;
-    }
-    
-    modifier notReverted() {
-        require(!revertFlag,'not permitted');
-        _;
-    }
-    
-    event Swap(address indexed from, address indexed to, address indexed tokenFrom, address tokenTo ,uint amountIn, uint amountGton, uint amountOut);
-    event CrossChainInput(bytes payload);
-    event CrossChainOutput(address indexed to, address indexed tokenTo, uint amountOut, uint gtonAmount);
-
-    constructor (
-    ) {
-
-        revertFlag = false;
-    }
-    
-    function setOwner(address _owner) public onlyOwner {
-        owner = _owner;
-    }
-    
-    function toggleRevert() public onlyOwner {
-        revertFlag = !revertFlag;
-    }
-    
-    function emergencyTokenTransfer(
-        address _token,
-        address _user,
-        uint _amount
-        
-    ) public {
-        require(IERC20(_token).transfer(_user,_amount),"Err");
-    }
-
-    
-    function crossChainFromEth (
-        uint16 chainType,
-        uint16 chainId,
-        uint _amountTokenIn,
-        bytes memory customPayload
-    ) public payable {
-        
-        bytes memory payload = abi.encodePacked(chainType,chainId,uint16(1),_amountTokenIn,customPayload);
-        //wormhole.lock(
-        //    chainType,
-        //    chainId,
-        //    payload
-        //);
-        //uint16 _chainType = chainType;
-        emit CrossChainInput(payload);
-    }
     
     function deserializeUint(
         bytes memory b,
@@ -92,15 +29,13 @@ contract OGSwap {
     
     function recv (
         bytes calldata payload
-    ) public payable {
+    ) public returns(uint chainFromType,uint chainFromId,uint gtonAmount,address payable receiver,address tokenTo) {
         
-        uint gtonAmount = deserializeUint(payload,6,6+32);
-        address payable receiver = payable(deserializeAddress(payload,6+32));
-        if (payload.length > 6+32+20) {
-            address tokenTo = deserializeAddress(payload,6+32+20);
-            emit CrossChainOutput(receiver, tokenTo, gtonAmount, gtonAmount);
-            return;
-        }
-        emit CrossChainOutput(receiver, address(gtonToken), gtonAmount, gtonAmount);
+         chainFromType = deserializeUint(payload,0,32);
+         chainFromId = deserializeUint(payload,32,32);
+         gtonAmount = deserializeUint(payload,32*2,32);
+         receiver = payable(deserializeAddress(payload,32*3));
+         tokenTo = deserializeAddress(payload,32*3+20);
+    
     }
 }
